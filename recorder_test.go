e88684411ca8a283da2d01f40af2b0e669b1b2ee
Yuri Shkuro <ys@uber.com>
2016-07-01 10:55:47 -0400

    Remove bad import (damn you IntelliJ)

--- recorder_test.go
+++ recorder_test.go
@@ -4,7 +4,8 @@ import (
 	"sync/atomic"
 	"testing"
 	"time"
-	"github.com/uber/ringpop-go/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestInMemoryRecorderSpans(t *testing.T) {
@@ -19,15 +20,6 @@ func TestInMemoryRecorderSpans(t *testing.T) {
 	apiRecorder.RecordSpan(span)
 	assert.Equal(t, []RawSpan{span}, recorder.GetSpans())
 	assert.Equal(t, []RawSpan{}, recorder.GetSampledSpans())
-	//if len(recorder.GetSpans()) != 1 {
-	//	t.Fatal("No spans recorded")
-	//}
-	//if !reflect.DeepEqual(recorder.GetSpans()[0], span) {
-	//	t.Fatal("Span not recorded")
-	//}
-	//if len(recorder.GetSampledSpans()) != 0 {
-	//	t.Fatal("Non-sampled span returned by GetSampledSpans")
-	//}
 }
 
 type CountingRecorder int32
